f365be8e5f2272069514bd5049beb2c36b038031
Charlie <66382380+T2ThatGuy@users.noreply.github.com>
2024-12-11 19:43:16 +0000

    feat: copy-config script now copies folders too

--- scripts/copy-config.ps1
+++ scripts/copy-config.ps1
@@ -1,10 +1,16 @@
 # All files and folders follow the format
 # (Source => Destination)
 
-$folders = @()
+$configDir = "./config"
+$folders = @{
+    "$configDir/glzr" = "$home/.glzr"
+    "$configDir/starship" = "$home/.starship"
+    "$configDir/fastfetch" = "$home/.config/fastfetch"
+    "$configDir/wezterm" = "$home/.config/wezterm"
+}
 
 $files = @{
-    "./config/powershell/Microsoft.PowerShell_profile.ps1" = $profile
+    "$configDir/powershell/Microsoft.PowerShell_profile.ps1" = $profile
 }
 
 Write-Host "--- Copying config (files)"
@@ -14,11 +20,27 @@ foreach ($file in $files.GetEnumerator()) {
         continue
     }
 
-    $fileExists = Test-Path $file.Value
+    $fileExists = Test-Path -Path $file.Value
     if ($fileExists -eq $False) {
         New-Item -ItemType File -Path $file.Value -Force | out-null
     }
 
     Copy-Item $file.key -Destination $file.Value -Force | out-null
     Write-Host "-- Copied file"$file.key"to"$file.Value
+}
+
+Write-Host "--- Copying config (folders)"
+foreach ($folder in $folders.GetEnumerator()) {
+    if ($dryrun -eq $True) {
+        Write-Host "- Skipping folder"$file.key". Reason: Dry run enabled"
+        continue
+    }
+
+    $folderExists = Test-Path -Path $folder.Value
+    if ($folderExists -eq $True) {
+        Remove-Item -Path $folder.Value -Recurse -Force | out-null
+    }
+
+    Copy-Item $folder.key -Destination $folder.Value -Recurse -Force | out-null
+    Write-Host "-- Copied folder"$folder.key"to"$folder.Value
 }
