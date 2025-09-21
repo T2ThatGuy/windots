2157d486eec120b3a4c26814bde73f6844a160f3
T2ThatGuy <66382380+T2ThatGuy@users.noreply.github.com>
2025-09-22 00:31:23 +0100

    task(setup): initial setup script updated
    Disabled for now whilst working on v2 infra

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,18 @@
+Import-Module "$PSscriptRoot/scripts/utils/logging.ps1" -Scope Local -Force
+
+$global:LogLevel = 0
+$global:dryrun = $false
+
+$global:ScriptsDirectory = "$PSscriptRoot/scripts"
+$global:UtilDirectory = "$ScriptsDirectory/utils"
 
 # Ask if this run should be a dry run or not (default Yes)
 $decision = $Host.UI.PromptForChoice('', 'Perform dryrun?', @('&Yes', '&No'), 0)
-if ($decision -eq 0) {
-    $dryrun = $true
-} else {
-    $dryrun = $false
-}
+if ($decision -eq 0) { $global:dryrun = $true }
 
-$scriptDir = "$PSscriptRoot\scripts"
-& "$scriptDir/install-apps.ps1"
-& "$scriptDir/copy-config.ps1"
-& "$scriptDir/first-time-setup.ps1"
-& "$scriptDir/setup-wsl.ps1"
+
+# $scriptDir = "$PSscriptRoot\scripts"
+# & "$scriptDir/install-apps.ps1"
+# & "$scriptDir/copy-config.ps1"
+# & "$scriptDir/first-time-setup.ps1"
+# & "$scriptDir/setup-wsl.ps1"
